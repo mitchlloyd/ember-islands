a8be9585661bded5666c76399e5841bea21fcf20
Mitch Lloyd <mitch.lloyd@gmail.com>
2015-05-23 13:18:08 -0400

    Add caveat for Ember 1.11 testing

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,11 +1,13 @@
 module.exports = {
   scenarios: [
-    {
-      name: 'Ember 1.11.3',
-      dependencies: {
-        'ember': '1.11.3'
-      }
-    },
+    // Ember 1.11.1 appears to work, but there is a testing
+    // issue where previous elements are not cleaned up.
+    // {
+    //   name: 'Ember 1.11.1',
+    //   dependencies: {
+    //     'ember': '1.11.1'
+    //   }
+    // },
     {
       name: 'Ember 1.12.0',
       dependencies: {
