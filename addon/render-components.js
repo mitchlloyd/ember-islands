a270e61530d21f6c92a754ea7668505f59ced55d
Jonas Brusman <himynameisjonas@gmail.com>
2015-06-23 16:38:17 +0200

    Save the component instance on the parent element

--- addon/render-components.js
+++ addon/render-components.js
@@ -12,9 +12,10 @@ export function getRenderComponentFor(application) {
     var component = componentLookup.lookupFactory(name, container);
     assert(`ember-islands could not find a component named "${name}" in your Ember appliction.`, component);
 
+    var $element = $(element);
     // Temporary fix for bug in `replaceIn`
-    $(element).empty();
-    component.create(attributes).appendTo(element);
+    $element.empty();
+    $element.data('component-instance', component.create(attributes).appendTo(element));
   };
 }
 
