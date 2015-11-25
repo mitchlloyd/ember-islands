66bc27d02a5320e35ef1e199a9538b02e219a210
Mitch Lloyd <mitch.lloyd@gmail.com>
2015-11-24 21:28:34 -0800

    Spike {{ember-islands}} component idea

---
+++ app/components/ember-islands.js
@@ -0,0 +1,53 @@
+import Ember from 'ember';
+const { $, getOwner, assert } = Ember;
+
+export default Ember.Component.extend({
+  tagName: '',
+
+  init() {
+    this._super(...arguments);
+    this.renderComponent = getRenderComponentFor(this);
+    this.componentsToRender = islandComponents();
+  },
+
+  didInsertElement() {
+    this.componentsToRender.forEach(this.renderComponent);
+  }
+});
+
+function componentAttributes(element) {
+  let attrs;
+  let attrsJSON = element.getAttribute('data-attrs');
+
+  if (attrsJSON) {
+    attrs = JSON.parse(attrsJSON);
+  } else {
+    attrs = {};
+  }
+
+  attrs.innerContent = element.innerHTML;
+  return attrs;
+}
+
+function getRenderComponentFor(emberObject) {
+  let componentLookup = getOwner(emberObject).lookup('component-lookup:main');
+
+  return function renderComponentWithComponentLookup({ name, attrs, element }) {
+    let component = componentLookup.lookupFactory(name);
+    assert(`ember-islands could not find a component named "${name}" in your Ember appliction.`, component);
+
+    component.create(attrs).replaceIn(element);
+  };
+}
+
+function islandComponents() {
+  let components = [];
+
+  $('[data-component]').each(function() {
+    let name = this.getAttribute('data-component');
+    let attrs = componentAttributes(this);
+    components.push({ attrs, name, element: this });
+  });
+
+  return components;
+}
